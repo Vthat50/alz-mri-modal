e23a178ab85c89b68341d842334cee195da6fb18
Vthat50 <67988368+Vthat50@users.noreply.github.com>
2025-04-02 19:30:11 -0700

    Update minimal.py

--- app/app/minimal.py
+++ app/app/minimal.py
@@ -1,1 +1,11 @@
+import modal
+from fastapi import FastAPI
 
+stub = modal.Stub("alz-test-minimal")
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+
+stub.app = app  # Critical for v0.73.141
